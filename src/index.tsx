d4dd44a6f34eb5387bf828d1af0eae4f8c7a8228
Jasper Haward <jasperhaward@virginmedia.com>
2022-03-13 14:02:46 +0000

    Optimise installed packages and bundle size.

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,17 @@ if (process.env.NODE_ENV === "development") {
 
 import { render, h } from "preact";
 import { setup } from "goober";
+
+// Import icons from respective directories to reduce bundle size,
+// webpack tree shaking seems to not work correctly otherwise
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {
-    faLinkedin,
-    faGithub,
-    faSteam,
-} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faSteam } from "@fortawesome/free-brands-svg-icons/faSteam";
 
 import App from "./App";
 import "./index.css";
-
-setup(h);
 
 library.add({
     faEnvelope,
@@ -23,5 +22,7 @@ library.add({
     faGithub,
     faSteam,
 });
+
+setup(h);
 
 render(<App />, document.body);
